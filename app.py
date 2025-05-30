71f9a3b50d1487f86be2bbe54e3bd32db3a67be6
Derrick Ankrah <darhin97@gmail.com>
2025-05-30 10:45:53 +0000

    add getting user feat

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ def connect_db():
 def get_db():
     if not hasattr(g, "sqlite_db"):
         g.sqlite_db = connect_db()
-        return g.sqlite_db
+    return g.sqlite_db
+
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, "sqlite_db"):
